c4d7bb07c52ef613de313cbdb7417144d66382d5
galoran <galoranbuisness@gmail.com>
2017-01-15 20:56:05 -0500

    Add files via upload

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,23 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+
 public class PlayerController : MonoBehaviour {
-	
-	private Ridgebody rb;
 
-	void start ()
+	public float speed;
 
+	private Rigidbody rb;
+
+	void Start ()
+	{
 		rb = GetComponent<Rigidbody>();
-
+	}
 
 	void FixedUpdate ()
-
+	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical")
+		float moveVertical = Input.GetAxis ("Vertical");
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f. moveVertical);	
-		
-		rb.AddForce (movement);	
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-
-
+		rb.AddForce (movement * speed);
+	}
+}
